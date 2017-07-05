d32255b37fbcedc4a5fd3087a0d0417b01af2f32
Avinash Shenoy <ashenoy@soasta.com>
2017-07-05 14:00:26 -0700

    Add cross origin resource sharing

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 // routes/index.js
 
 const notesRoutes = require('./note_routes');
+const authRoutes = require('./auth_routes');
 
 module.exports = function(app, db)
 {
     notesRoutes(app, db);
+    authRoutes(app, db);
 
 };
--- server.js
+++ server.js
@@ -3,13 +3,15 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const db = require('./config/db');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true }));
+app.use(cors());
+
 
 const port = 5000;
-//const port = 5858;
 
 MongoClient.connect(db.url, (err, database) =>
 {
